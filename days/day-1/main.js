ed8862e3a50a8c6d684865134d430000c6b1c1ee
Reda <rmg7fm@gmail.com>
2026-09-10 15:38:41 +0300

    Code

---
+++ days/day-1/main.js
@@ -0,0 +1,20 @@
+// Hi in My First File in JavaScript //
+
+// This comment in one line
+
+/* This comment 
+in multi lines */
+
+alert("Hello World!");
+console.log("Hello World!");
+
+// the " ; " isn't very important in end line in code, but is very important in this example :
+
+console.log("Test ;");
+[Hello, World].forEach(alert);
+
+// in line { console.log("Test ;"); } the ; is very important to run this code 
+
+
+// I'm sorry, my English is not good
+// Thank You!
