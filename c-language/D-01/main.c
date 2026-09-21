aa474681184555a155ea62b8d94e2efba5336046
Reda <rmg7fm@gmail.com>
2026-09-21 22:33:56 +0300

    Day 1 in C

---
+++ c-language/D-01/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  // This commint
+  /* And this Commint */
+  
+  printf("Hi in my first code in C language\n");
+  int countString;
+  countString = printf("I'm Reda, I'm from Iraq\n");
+  printf("Count of letters is : %d .", countString);
+  
+  printf("\n%10s\n", "Hi");
+  printf("%-10s World", "Hello");
+  printf("\n%p\n", &countString);
+  printf("%05d\n", 7);
+  printf("GoodBye\n");
+  return 0;
+}
